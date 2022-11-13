a5bc1eae17728ff0f835147cf66bb0af308a04f0
Jeevan Joshi <joshijeevan98@gmail.com>
2022-11-13 19:00:54 +0530

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,19 @@
 import 'package:logster/logster.dart';
 
-void main() {}
+void main() {
+  const a = 10;
+  const b = 2.0;
+  const c = 'x';
+  const d = true;
+  final e = [a, b];
+  final f = {c, d};
+  final g = {a: b, c: d};
+
+  a.logs; // [int] 10
+  b.logs; // [double] 2.0
+  c.logs; // [String] x
+  d.logs; // [bool] true
+  e.logs; // [List<num>] [10, 2.0]
+  f.logs; // [CompactImmutableLinkedHashSet<Object>] {x, true}
+  g.logs; // [InternalImmutableLinkedHashMap<Object, Object>] {10: 2.0, x: true}
+}
